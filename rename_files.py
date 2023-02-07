778da31bc9701ead6f2d6a6eff0f3b1bf52bdf77
mgoldkuhle <goldkuhle@uni-bonn.de>
2023-02-07 17:54:27 +0100

    restructure repo

--- rename_files.py
+++
@@ -1,9 +0,0 @@
-# to rename all .jpg images in a folder with an ascending trailing index
-
-import os
-path = 'data/crops_256_cond/ffhq/'
-files = os.listdir(path)
-
-
-for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, 'renamed', ''.join(['ffhq', str(index), '.jpg'])))
